424129886d1f98520c782cfc2c9a5072d31390a8
liupei <lp@@2016>
2016-09-20 17:38:08 +0800

    new

--- app/src/main/java/com/swiperefreshrecyclerview/SwipeRefreshActivity.java
+++ app/src/main/java/com/swiperefreshrecyclerview/SwipeRefreshActivity.java
@@ -1,6 +1,7 @@
 package com.swiperefreshrecyclerview;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Administrator on 2016/9/20.
@@ -8,4 +9,9 @@ import android.app.Activity;
 
 public class SwipeRefreshActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.swipe_refresh_activity);
+    }
 }
