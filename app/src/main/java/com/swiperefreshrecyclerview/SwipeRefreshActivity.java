5189bfef2b50bfe55dee55d47479eb9068ed3ca0
liupei <lp@@2016>
2016-09-20 17:06:29 +0800

    new

---
+++ app/src/main/java/com/swiperefreshrecyclerview/SwipeRefreshActivity.java
@@ -0,0 +1,11 @@
+package com.swiperefreshrecyclerview;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2016/9/20.
+ */
+
+public class SwipeRefreshActivity extends Activity {
+
+}
